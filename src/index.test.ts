e2036ec479a716241b2bd6fd4798ff756f7eb404
David Snyder <davidgraygs4@gmail.com>
2020-09-24 20:11:13 -0400

    fix issue with intersectionObserver in tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,34 +1,14 @@
 import { useIntersectionObserver } from './'
 import { renderHook } from "@testing-library/react-hooks";
 
-// Testing will be done later
-
-// mock timer using jest
 jest.useFakeTimers();
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
+describe('useIntersectionObserver', () => {
+  it('hook returns the right values', () => {
     const value = renderHook(() => useIntersectionObserver(entries => {
       console.log(entries)
     }, {}))
 
     expect(value).toBeDefined()
-    // expect(result.current).toBe(0);
-
-    // // Fast-forward 1sec
-    // act(() => {
-    //   jest.advanceTimersByTime(1000);
-    // });
-
-    // // Check after total 1 sec
-    // expect(result.current).toBe(1);
-
-    // // Fast-forward 1 more sec
-    // act(() => {
-    //   jest.advanceTimersByTime(1000);
-    // });
-
-    // // Check after total 2 sec
-    // expect(result.current).toBe(2);
   })
 })
